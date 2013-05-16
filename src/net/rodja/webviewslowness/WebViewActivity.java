f3206dbe69ce7e26c9f285006eb096aad310b969
Rodja Trappe <mail@rodja.net>
2013-05-16 06:16:18 +0200

    demo code to show the problem with webview's slow rendering

---
+++ src/net/rodja/webviewslowness/WebViewActivity.java
@@ -0,0 +1,26 @@
+package net.rodja.webviewslowness;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class WebViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        WebView webview = (WebView) findViewById(R.id.webview);
+        webview.loadData("<html><head></head><body>WebView rendering is slow on some devices like the Nexus 4!<br><br>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</body></html>", "text/html", "utf-8");
+    }
+   
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_web_view, menu);
+        return true;
+    }
+    
+}
